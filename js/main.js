39f0409be2e161b6a96b6d8dfdb4d1895cfd8ae4
Lisa Trevis <l_trevis@hotmail.com>
2018-05-18 21:41:02 -0700

    completed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
 // ----- Promises & Fetch ------
+
+// promise = new Promise( (resolve, reject) => {
+// 	setTimeout(() => {
+// 		resolve();
+// 	}, 3000);
+// });
+
+// // There can be more than one callback function
+// promise
+// 	.then(() => console.log("Finally done!"))
+// 	.then(() => console.log("I also finished!"))
+// 	.catch(() => console.log("Rejected!"))
+
+
+// Here's a practical example of an ajax request with fetch:
+url = "https://jsonplaceholder.typicode.com/posts/";
+
+fetch(url)
+	.then(response => response.json())
+	.then(data => console.log(data));
+
+
+
+
+
+
+
+
+
+
+
+
+
